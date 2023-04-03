25f1925ffb3654b792288282a4b7b318dd485e5d
MelospizaStartUp <sathyasivamloganathan@gmail.com>
2023-04-03 17:36:08 +0530

    Initial commit

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ import Logo from '../../assests/logo.png'
 const Footer = () => {
   return (
     <div style={{background:'#fff', width:'100%', height:'300px'}}>
-        <img src={Logo} className='Logo' alt='Melospiza'/><h1 className='Footer__title'>Melospiza&trade;</h1>
+        <img src={Logo} className='Logo' alt='Melospiza'/><h1 className='Footer__title'>Melospiza&#174;</h1>
         <Col className='icon__group'>
         <a class='mail' href="mailto: official.melospiza@gmail.com" style={{textDecoration:'none'}}><i className='ri-mail-send-line icon'></i></a>
             <a href="https://www.instagram.com/melospiza.team/" target="blank" style={{textDecoration:'none'}}><i className='ri-instagram-line icon'></i></a>
